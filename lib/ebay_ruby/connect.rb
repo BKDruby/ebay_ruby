e50f5b1b3840c09b2ce419b7bea0b83ce947e198
Kirill Berlin <kirill.b.as@agiliumlabs.com>
2018-01-24 23:18:44 +0200

    add page number

--- lib/ebay_ruby/connect.rb
+++ lib/ebay_ruby/connect.rb
@@ -15,15 +15,15 @@ class EbayConnect
     response.body
   end
 
-  def find_items_by_keywords(search_keyword, per_page)
+  def find_items_by_keywords(search_keyword, per_page, page_number = 1)
     str = __method__.to_s.camelize
     operation_name = str.gsub(str.first, str.first.downcase)
     search_param = "keywords=#{URI::encode(search_keyword)}"
-    ebay_items = EbayFindItem.new(get_connect(build_uri(operation_name, search_param, per_page)))
+    ebay_items = EbayFindItem.new(get_connect(build_uri(operation_name, search_param, per_page, page_number)))
     ebay_items.all_items
   end
 
-  def find_items_by_category(category_id, per_page)
+  def find_items_by_category(category_id, per_page, page_number = 1)
     str = __method__.to_s.camelize
     operation_name = str.gsub(str.first, str.first.downcase)
     search_param = "categoryId=#{category_id}"
@@ -31,7 +31,7 @@ class EbayConnect
     ebay_items.all_items
   end
   #productId can be ISBN,UPC,EAN
-  def find_items_by_product(product_id, per_page)
+  def find_items_by_product(product_id, per_page, page_number = 1)
     str = __method__.to_s.camelize
     operation_name = str.gsub(str.first, str.first.downcase)
     search_param = "productId=#{product_id}"
@@ -40,7 +40,7 @@ class EbayConnect
   end
 
   private
-  def build_uri(operation_name, search_keyword, per_page)
+  def build_uri(operation_name, search_keyword, per_page, page_number = 1)
     uri_string = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=#{operation_name}" \
                  "&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=#{app_id}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON" \
                  "&#{search_keyword}&paginationInput.entriesPerPage=#{per_page.to_i}" \
